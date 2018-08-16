a6faea861fa4256022de77c70030d89dcae722a8
0918nobita <nobita.0918@gmail.com>
2018-08-16 12:10:11 +0900

    Initial commit

---
+++ app/src/main/java/zero918nobita/net/kyotob/MainActivity.kt
@@ -0,0 +1,12 @@
+package zero918nobita.net.kyotob
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
