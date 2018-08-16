ff44cc5cd82456b033ecaf938bcc7f08bb5e982c
0918nobita <nobita.0918@gmail.com>
2018-08-16 15:23:54 +0900

    ボタンを押すとページ遷移するようにした

--- app/src/main/java/zero918nobita/net/kyotob/MainActivity.kt
+++ app/src/main/java/zero918nobita/net/kyotob/MainActivity.kt
@@ -1,12 +1,18 @@
 package zero918nobita.net.kyotob
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        startActivity(Intent(this, ChatActivity::class.java))
     }
 }
