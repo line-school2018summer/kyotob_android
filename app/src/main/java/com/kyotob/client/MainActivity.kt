ca6feec32ebd73fd9a1bec53436ad42ebee96b92
0918nobita <nobita.0918@gmail.com>
2018-08-30 18:02:00 +0900

    :sparkles: MainActivity に、ChatActivity に遷移するボタンを追加した

--- app/src/main/java/com/kyotob/client/MainActivity.kt
+++ app/src/main/java/com/kyotob/client/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onClick(view: View) {
+    fun openChatActivity(view: View) {
         startActivity(Intent(this, ChatActivity::class.java))
     }
 }
