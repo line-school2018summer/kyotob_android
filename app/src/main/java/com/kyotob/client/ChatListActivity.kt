dbcf838a5a11075059ad0a7f3b91564212767407
0918nobita <nobita.0918@gmail.com>
2018-08-30 17:48:31 +0900

    :sparkles: ChatListActivity を追加した

---
+++ app/src/main/java/com/kyotob/client/ChatListActivity.kt
@@ -0,0 +1,12 @@
+package com.kyotob.client
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_list)
+    }
+}
