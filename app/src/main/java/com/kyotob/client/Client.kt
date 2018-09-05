b64774355fdaa073639eec4ecda9f9a56afdcc49
0918nobita <nobita.0918@gmail.com>
2018-09-05 16:25:08 +0900

    :sparkles: メッセージ取得APIと連携するための Retrofit2 の設定を追加した
    ClientインターフェースにgetMessagesメソッドを追加し、アノテーションでurlのパターンと引数への渡し方を指定した

--- app/src/main/java/com/kyotob/client/Client.kt
+++ app/src/main/java/com/kyotob/client/Client.kt
@@ -1,10 +1,15 @@
 package com.kyotob.client
 
+import com.kyotob.client.entities.Message
 import com.kyotob.client.entities.User
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Client {
     @GET("/user")
     fun getUser(@Query("id") query: String): User?
+
+    @GET("/room/{room_id}/messages")
+    fun getMessages(@Path("room_id") roomId: Int): Array<Message>?
 }
