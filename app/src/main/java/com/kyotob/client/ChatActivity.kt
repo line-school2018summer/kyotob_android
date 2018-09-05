41673c13f344e34ee6feb67a5f9beee9ec245e62
0918nobita <nobita.0918@gmail.com>
2018-09-05 18:04:42 +0900

    :sparkles: チャット画面でダミーのメッセージのリストを描画するようにした

--- app/src/main/java/com/kyotob/client/ChatActivity.kt
+++ app/src/main/java/com/kyotob/client/ChatActivity.kt
@@ -2,6 +2,10 @@ package com.kyotob.client
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.kyotob.client.adapter.MessageListAdapter
+import com.kyotob.client.entities.Message
+import java.sql.Timestamp
 
 class ChatActivity : AppCompatActivity() {
 
@@ -9,5 +13,16 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         title = "チャット"
+
+        val listAdapter = MessageListAdapter(applicationContext)
+        listAdapter.messages = getDummyMessages()
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        listView.adapter = listAdapter
     }
+
+    private fun getDummyMessages() =
+            mutableListOf(
+                    Message(1, 1, 1, "foo", Timestamp(1536133785)),
+                    Message(1, 2, 1, "bar", Timestamp(1536137278)))
 }
