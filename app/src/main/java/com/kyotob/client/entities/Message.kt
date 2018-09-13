e172fdf2994736b1f4387257dec2fe513bfd1f6f
0918nobita <nobita.0918@gmail.com>
2018-09-13 17:39:47 +0900

    :sparkles: Add PostMessageRequest data class

--- app/src/main/java/com/kyotob/client/entities/Message.kt
+++ app/src/main/java/com/kyotob/client/entities/Message.kt
@@ -12,3 +12,6 @@ data class GetMessageResponse(val createdAt: Timestamp,
                               val userName: String,
                               val userScreenName: String,
                               val content: String)
+
+data class PostMessageRequest(val user_name: String,
+                              val content: String)
