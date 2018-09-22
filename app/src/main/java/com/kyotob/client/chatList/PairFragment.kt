2f561c7875b888bf22a52cd16d48089b2494efd2
kodaig <kodaig06@gmail.com>
2018-09-22 17:39:56 +0900

    音波通信の送信部を実装

--- app/src/main/java/com/kyotob/client/chatList/PairFragment.kt
+++ app/src/main/java/com/kyotob/client/chatList/PairFragment.kt
@@ -1,5 +1,6 @@
 package com.kyotob.client.chatList
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.content.Context
@@ -9,10 +10,7 @@ import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.kyotob.client.*
@@ -26,6 +24,21 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
+//音波用のインポート
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioRecord
+import android.media.AudioTrack
+import android.os.Handler
+import android.os.Message
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.EditText
+import android.widget.ToggleButton
+import com.kyotob.client.R
+import java.io.UnsupportedEncodingException
+import java.util.*
+
 class PairFragment: Fragment() {
 
     lateinit var dialogEditText: EditText
@@ -33,6 +46,8 @@ class PairFragment: Fragment() {
     lateinit var notFoundView: TextView
     lateinit var foundView: ConstraintLayout
     lateinit var foundText: TextView
+    lateinit var receiveBtn: Button
+    lateinit var sendBtn: ToggleButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.dialog_pair, null)
@@ -44,6 +59,11 @@ class PairFragment: Fragment() {
             notFoundView = findViewById(R.id.dialog_not_found_text_view)
             foundView = findViewById(R.id.dialog_found_user)
             foundText = findViewById(R.id.dialog_user_name_text_view)
+
+            //音波通信ボタン
+            receiveBtn = findViewById(R.id.soundReceiveBtn)
+            sendBtn = findViewById(R.id.soundSendBtn)
+
         }
 
         /* JSON のスネークケースで表現されるフィールド名を、
@@ -127,6 +147,142 @@ class PairFragment: Fragment() {
             // ダイアログを閉じる
             //dialog.dismiss()
             //activity?.recreate()
+        }
+
+        ///音波通信用の実装BELOW///
+
+        ///////
+        val SAMPLE_RATE = 44100
+        val SEC_PER_SAMPLEPOINT = 1.0f / SAMPLE_RATE
+        val AMP = 4000
+        val FREQ_BASE = 1000
+        val FREQ_STEP = 20
+        val FREQ_KEY = FREQ_BASE - 20
+        val ELMS_1SEC = SAMPLE_RATE
+        val ELMS_100MSEC = SAMPLE_RATE / 10
+        val ELMS_MAX = 256
+
+        val PLAY_START = 120
+        val PLAY_END = 130
+
+        val mPlayBuf = ShortArray(SAMPLE_RATE)
+        val mSignals = Array(ELMS_MAX) { ShortArray(SAMPLE_RATE / 10) }
+
+        val bufferSizeInBytes = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT)
+
+        //再生用
+        var mAudioTrack = AudioTrack(AudioManager.STREAM_MUSIC,
+                SAMPLE_RATE,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSizeInBytes,
+                AudioTrack.MODE_STREAM)
+
+        // サイン波データを生成
+        fun createSineWave(buf: ShortArray, freq: Int, amplitude: Int, doClear: Boolean) {
+            if (doClear) {
+                Arrays.fill(buf, 0.toShort())
+            }
+            for (i in buf.indices) {
+                val currentSec = i * SEC_PER_SAMPLEPOINT // 現在位置の経過秒数
+                val `val` = amplitude * Math.sin(2.0 * Math.PI * freq.toDouble() * currentSec.toDouble())
+                buf[i] = `val`.toShort()
+            }
+        }
+
+        // 指定されたバイト値を音声信号に置き換えて再生する
+        fun valueToWave(`val`: Byte) {
+            mAudioTrack.write(mSignals[`val`.toInt()], 0, ELMS_100MSEC)
+        }
+
+        //音波用ハンドラー
+        class iHandler(): Handler() {
+            override fun handleMessage(msg: Message) {
+                when(msg.what){
+                    PLAY_END -> {
+                        sendBtn.isChecked = false
+                    }
+                }
+            }
+        }
+
+        val mHandler = iHandler()
+
+        //音波用Runnable
+        class sendRun: Runnable{
+            override fun run() {
+                // 先頭・終端の目印用信号データ
+                createSineWave(mPlayBuf, FREQ_KEY, AMP, true)
+
+                // 256種類の信号データを生成
+                for (i in 0 until ELMS_MAX) {
+                    createSineWave(mSignals[i], (FREQ_BASE + FREQ_STEP * i).toShort().toInt(), AMP, true)
+                }
+
+                //TODO Preferenceから自分のID取得
+                val myId = dialogEditText.text.toString()
+                val strByte: ByteArray = myId.toByteArray(charset("UTF-8"))
+
+                mAudioTrack.play()
+                mAudioTrack.write(mPlayBuf, 0, ELMS_1SEC/2) // 開始
+                for (i in strByte.indices) {
+                    valueToWave(strByte[i])
+                }
+                mAudioTrack.write(mPlayBuf, 0, ELMS_1SEC/2) // 終端
+
+                mAudioTrack.stop()
+                mAudioTrack.flush()
+                mHandler.sendEmptyMessage(PLAY_END)
+            }
+        }
+
+        //音波で自分のIDを送信
+        sendBtn.setOnClickListener{
+            if (mAudioTrack.playState != AudioTrack.PLAYSTATE_STOPPED) {
+                mAudioTrack.stop()
+                mAudioTrack.flush()
+            }
+            if (sendBtn.isChecked) {
+                Thread(sendRun()).start()
+            }
+        }
+
+        //音波でIDを取得
+        receiveBtn.setOnClickListener{
+            //TODO 音波受け取ってdialogEditTextにセット
+
+            // 通信
+            client.searchUser(dialogEditText.text.toString(), token).enqueue(object : Callback<SearchUserResponse> {
+                // Request成功時に呼ばれる
+                override fun onResponse(call: Call<SearchUserResponse>, response: Response<SearchUserResponse>) {
+
+                    // 通信成功時
+                    if(response.isSuccessful) {
+                        // TEST
+                        // ユーザー表示名の変更
+                        foundText.text = response.body()!!.screenName
+
+                        foundView.visibility = View.VISIBLE
+                        notFoundView.visibility = View.INVISIBLE
+                    }
+                    // Bad request
+                    else {
+                        foundView.visibility = View.INVISIBLE
+                        notFoundView.visibility = View.VISIBLE
+                    }
+                }
+
+                // Request失敗時に呼ばれる
+                override fun onFailure(call: Call<SearchUserResponse>?, t: Throwable?) {
+                    // Fail to connect Internet access
+                    if (activity != null) {
+                        Toast.makeText(activity?.applicationContext, "Fail to Connect Internet Access", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+
         }
         return root
     }
