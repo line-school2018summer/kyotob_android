de195ea4524d2b46efe4a238b1cd5610a88a0e5d
0918nobita <nobita.0918@gmail.com>
2018-09-23 20:53:35 +0900

    :art: 必要ない Message データクラスを削除した

--- app/src/main/java/com/kyotob/client/entities/Message.kt
+++ app/src/main/java/com/kyotob/client/entities/Message.kt
@@ -2,12 +2,6 @@ package com.kyotob.client.entities
 
 import java.sql.Timestamp
 
-data class Message(val id: Int,
-                   val senderId: Int,
-                   val roomId: Int,
-                   val content: String,
-                   val created: Timestamp)
-
 data class GetMessageResponse(val createdAt: Timestamp,
                               val userName: String,
                               val userScreenName: String,
